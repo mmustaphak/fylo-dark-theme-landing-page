604d4a07b3e61e32521cd8af952e5394be019de4
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-11-28 16:13:13 +0530

    Added Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Nav from "./components/Nav.jsx"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Nav/>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+const nav = ()=>{
+    return(
+        <nav>
+            <h1>Nav here</h1>
+        </nav>
+    )
+}
+
+export default nav
