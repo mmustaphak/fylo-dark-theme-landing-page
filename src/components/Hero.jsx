3e735f759df3fdbaa679613e0eb53cff74cf4e45
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-11-29 15:37:59 +0530

    Added basic html for hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
+import illustrationIntro from "../assets/illustration-intro.png"
+
 const Hero = ()=>{
     return(
-        <>
-            Hero here
-        </>
+        <main>
+            <img src={illustrationIntro} alt="" />
+            <h1>All your files in one secure locations, accessible anywhere.</h1>
+            <p>Fylo stores all your most important files in one secure locations. Access them wherever you need, share and collaborate wit friends family, anc co-workers</p>
+            <button>Get Started</button>
+        </main>
     )
 }
 
