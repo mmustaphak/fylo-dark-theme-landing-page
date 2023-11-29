21c591775e165b4e71600ec189cb0d7e6fffc22b
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-11-29 16:43:30 +0530

    Added basic styling to hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,16 @@
 import illustrationIntro from "../assets/illustration-intro.png"
-
+import mobileCurve from "../assets/bg-curvy-mobile.svg"
 const Hero = ()=>{
     return(
-        <main>
-            <img src={illustrationIntro} alt="" />
-            <h1>All your files in one secure locations, accessible anywhere.</h1>
-            <p>Fylo stores all your most important files in one secure locations. Access them wherever you need, share and collaborate wit friends family, anc co-workers</p>
-            <button>Get Started</button>
+        <main className={"bg-[#1C2431]"}>
+            <div className="p-8">
+                <img src={illustrationIntro} className="" alt="" />
+            </div>
+            <div className="p-8  text-white text-center bg-[#181F2A]">
+                <h1 className="my-4 text-2xl font-bold">All your files in one secure locations, accessible anywhere.</h1>
+                <p>Fylo stores all your most important files in one secure locations. Access them wherever you need, share and collaborate wit friends family, anc co-workers</p>
+                <button>Get Started</button>
+            </div>
         </main>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/logo.svg"
 const nav = ()=>{
     return(
-        <nav className="flex items-center justify-between p-4 bg-[#181F2A]">
+        <nav className="flex items-center justify-between p-4 bg-[#1C2431]">
             <img className="w-20" src={logo} alt="" />
             <ul className="text-white flex justify-between w-[60%] max-w-[15rem]">
                 <li>Features</li>
