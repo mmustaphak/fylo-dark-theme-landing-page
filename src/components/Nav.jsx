5493c816c0b2916f08a77604d5455926fd95942c
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-11-30 12:47:25 +0530

    Updated nav component to use color from tailwind config

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/logo.svg"
 const nav = ()=>{
     return(
-        <nav className="flex items-center justify-between p-4 bg-[#1C2431]">
+        <nav className="flex items-center justify-between p-4 bg-intro">
             <img className="w-20" src={logo} alt="" />
             <ul className="text-white flex justify-between w-[60%] max-w-[15rem]">
                 <li>Features</li>
