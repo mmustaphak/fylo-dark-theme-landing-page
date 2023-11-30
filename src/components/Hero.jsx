41e13d7c267bbf8488997d5c817a7635293db6be
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-11-30 12:54:43 +0530

    Updated colors on Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import illustrationIntro from "../assets/illustration-intro.png"
 
 const Hero = ()=>{
     return(
-        <main className={"bg-[#1C2431]"}>
+        <main className={"bg-intro"}>
             <div className="p-8">
                 <img src={illustrationIntro} className="" alt="" />
             </div>
-            <div className="p-8  text-white text-center bg-[#181F2A]">
+            <div className="p-8  text-white text-center bg-main">
                 <h1 className="my-4 text-2xl font-bold">All your files in one secure locations, accessible anywhere.</h1>
                 <p>Fylo stores all your most important files in one secure locations. Access them wherever you need, share and collaborate wit friends family, anc co-workers</p>
                 <button className="bg-gradient-to-r">Get Started</button>
