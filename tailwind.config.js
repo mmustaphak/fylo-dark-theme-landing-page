22a37c1256c2450c20250410ab28a842f9ad549b
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-01 16:30:43 +0530

    Added color gradient using tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ export default {
         main: 'hsl(var(--blue-main)/ <alpha-value>)',
         footer: 'hsl(var(--blue-footer)/ <alpha-value>)',
         testimonial: 'hsl(var(--blue-testimonial)/ <alpha-value>)',
-        intro: 'hsl(var(--blue-intro)/ <alpha-value>)'
+        intro: 'hsl(var(--blue-intro)/ <alpha-value>)',
+        'call-to-action-gradient1': 'hsl(var(--call-to-action-gradient1)/ <alpha-value>)',
+        'call-to-action-gradient2': 'hsl(var(--call-to-action-gradient2)/ <alpha-value>)'
       },
       backgroundImage:{
         'mobile-curve':"url('./src/assets/bg-curvy-mobile.svg')",
