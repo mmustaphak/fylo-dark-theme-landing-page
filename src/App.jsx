d3f2ddfd155672a2f532907067f2c36342b3d8ae
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-03 16:02:04 +0530

    Added Additional Info componeent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Nav from "./components/Nav.jsx"
 import Hero from "./components/Hero.jsx"
+import AdditionalInfo from "./components/AdditionalInfo.jsx"
+
 export default function App() {
   return (
     <>
       <Nav/>
       <Hero/>
+      <AdditionalInfo/>
     </>
   )
 }
---
+++ src/components/AdditionalInfo.jsx
@@ -0,0 +1,9 @@
+const AdditionalInfo = ()=>{
+    return(
+        <section>
+            Additionl Info
+        </section>
+    )
+}
+
+export default AdditionalInfo
