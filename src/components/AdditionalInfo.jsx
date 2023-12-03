f750c4ef3b7c857a69e91d8a855e2dc7c4c731db
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-03 16:22:30 +0530

    Added basic html to AdditonalInfo component

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -1,7 +1,37 @@
-const AdditionalInfo = ()=>{
-    return(
+import security from "../assets/icon-security.svg"
+import collaboration from "../assets/icon-collaboration.svg"
+import access from "../assets/icon-access-anywhere.svg"
+import file from "../assets/icon-any-file.svg"
+
+const AdditionalInfo = () => {
+    return (
         <section>
-            Additionl Info
+            
+            <div>
+                <img src={access} alt="Acess anywhere" />
+                <h3>Access your files, anywhere</h3>
+                <p>The ability to use a smartphone, tablet, or computer to access your account mean your files follow you everywhere.</p>
+            </div>
+
+            <div>
+                <img src={security} alt="Shield" />
+                <h3>Security you can trust</h3>
+                <p>The ability  to use a smartphone, tablet, or computer to acess your account mean your files follow you everywhere. </p>
+            </div>
+
+            <div>
+                <img src={collaboration} alt="Efficient Collaboration" />
+                <h3>Real-time collaboration</h3>
+                <p>SEcurely share files and folders with friends, family and colleagues for live collaboration. No email attachments required
+                </p>
+            </div>
+
+
+            <div>
+                <img src={file} alt="Folder" />
+                <h3>Store any type of file</h3>
+                <p>Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to securely stored and shared.</p>
+            </div>
         </section>
     )
 }
