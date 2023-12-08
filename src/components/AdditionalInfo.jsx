96ad2419cf995f2f4c12664063091016801c9231
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-08 16:02:34 +0530

    Made the AdditionalInfo more responsive

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -5,32 +5,34 @@ import file from "../assets/icon-any-file.svg"
 
 const AdditionalInfo = () => {
     return (
-        <section className="grid gap-16 justify-center px-4 pt-32 text-center text-white bg-main">
-            
-            <div>
-                <img src={access} className="mx-auto mb-10" alt="Acesss anywhere" />
-                <h3 className="mb-3 text-lg font-bold font-raleway">Access your files, anywhere</h3>
-                <p>The ability to use a smartphone, tablet, or computer to access your account mean your files follow you everywhere.</p>
-            </div>
+        <section className="text-center text-white bg-main">
+            <div className="grid gap-16 mx-auto px-4 pt-32 max-w-sm">
+                <div>
+                    <img src={access} className="mx-auto mb-10" alt="Acesss anywhere" />
+                    <h3 className="mb-3 text-lg font-bold font-raleway">Access your files, anywhere</h3>
+                    <p>The ability to use a smartphone, tablet, or computer to access your account mean your files follow you everywhere.</p>
+                </div>
 
-            <div>
-                <img src={security} className="mx-auto mb-10" alt="Shield" />
-                <h3 className="mb-3 text-lg font-bold font-raleway">Security you can trust</h3>
-                <p>The ability  to use a smartphone, tablet, or computer to acess your account mean your files follow you everywhere. </p>
-            </div>
+                <div>
+                    <img src={security} className="mx-auto mb-10" alt="Shield" />
+                    <h3 className="mb-3 text-lg font-bold font-raleway">Security you can trust</h3>
+                    <p>The ability  to use a smartphone, tablet, or computer to acess your account mean your files follow you everywhere. </p>
+                </div>
 
-            <div>
-                <img src={collaboration} className="mx-auto mb-10" alt="Efficient Collaboration" />
-                <h3 className="mb-3 text-lg font-bold font-raleway">Real-time collaboration</h3>
-                <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required
-                </p>
-            </div>
+                <div>
+                    <img src={collaboration} className="mx-auto mb-10" alt="Efficient Collaboration" />
+                    <h3 className="mb-3 text-lg font-bold font-raleway">Real-time collaboration</h3>
+                    <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required
+                    </p>
+                </div>
 
 
-            <div>
-                <img src={file} className="mx-auto mb-10" alt="Folder" />
-                <h3 className="mb-3 text-lg font-bold font-raleway">Store any type of file</h3>
-                <p>Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to securely stored and shared.</p>
+                <div>
+                    <img src={file} className="mx-auto mb-10" alt="Folder" />
+                    <h3 className="mb-3 text-lg font-bold font-raleway">Store any type of file</h3>
+                    <p>Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to securely stored and shared.</p>
+                </div>
+
             </div>
         </section>
     )
