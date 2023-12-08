3f9244f53f8fe6badc868023d91a5fc048758290
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-08 16:05:21 +0530

    Added Collaboration component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav.jsx"
 import Hero from "./components/Hero.jsx"
 import AdditionalInfo from "./components/AdditionalInfo.jsx"
+import Collaboration from "./components/Collaboration.jsx"
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Nav/>
       <Hero/>
       <AdditionalInfo/>
+      <Collaboration/>
     </>
   )
 }
---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,9 @@
+const Collaboration = () => {
+    return (
+        <section>
+            <h1>Collaboration--</h1>
+        </section>
+    )
+}
+
+export default Collaboration
