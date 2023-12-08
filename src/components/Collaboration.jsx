c5f5e37d67995267f7cd4757aafef4321f3f4107
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-08 16:23:58 +0530

    Added basic HTML for collaboration componenet

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,7 +1,18 @@
+import collabImg from "../assets/illustration-stay-productive.png"
+import arrow from "../assets/icon-arrow.svg"
+
 const Collaboration = () => {
     return (
-        <section>
-            <h1>Collaboration--</h1>
+        <section className="">
+            <img src={collabImg} alt="Collaboration" />
+            <h1>Stay productive, wherever you are</h1>
+            <p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
+            <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
+            <div>
+                <a href="#">See how Fylo works</a>
+                <img src={arrow} alt="" />
+            </div>
+
         </section>
     )
 }
