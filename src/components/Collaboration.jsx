4da00948c9d1fe643e62ea9925d14a167c10bde9
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-10 12:26:34 +0530

    Added overlall styling to Collaboration component

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -3,16 +3,17 @@ import arrow from "../assets/icon-arrow.svg"
 
 const Collaboration = () => {
     return (
-        <section className="">
-            <img src={collabImg} alt="Collaboration" />
-            <h1>Stay productive, wherever you are</h1>
-            <p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
-            <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
+        <section className="bg-main pt-32 text-white px-3.5">
             <div>
-                <a href="#">See how Fylo works</a>
-                <img src={arrow} alt="" />
+                <img  src={collabImg} alt="Collaboration" />
+                <h1 className="mt-10 font-bold text-xl text-center">Stay productive, wherever you are</h1>
+                <p className="mt-4 text-left">Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
+                <p className="mt-4 text-left">Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
+                <div className="flex items-center justify-between max-w-[10.5rem] mt-4 pb-1 text-call-to-action-gradient1 border-b-[1px] border-call-to-action-gradient1">
+                    <a href="#">See how Fylo works</a>
+                    <img src={arrow} alt="" />
+                </div>
             </div>
-
         </section>
     )
 }
