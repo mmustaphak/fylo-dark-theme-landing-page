17660a6cac1241cb763727b0e2d4bf44577e0c51
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-11 16:37:09 +0530

    Updated the mapped array to contain both image and name of the testimonial author

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,15 +5,20 @@ import quotes from "../assets/bg-quotes.png"
 
 const Testimonials = () => {
 
-    const images = [profile1, profile2, profile3]
-    const testimonials = images.map((image) => {
+    const data = [
+        {name: "Satish Patel", image: profile1},
+        {name: "Bruce McKenzie", image: profile2},
+        {name: "Iva Boyd", image: profile3}
+    ]
+
+    const testimonials = data.map((person) => {
         return(
-            <div key={image} className="px-6 py-8 bg-testimonial rounded-md mt-6">
+            <div key={person} className="px-6 py-8 bg-testimonial rounded-md mt-6">
                 <p className="text-sm">Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.</p>
                 <div className="flex items-center mt-4 text-xs">
-                    <img className="w-8 h-8 rounded-full" src={image} alt="" />
+                    <img className="w-8 h-8 rounded-full" src={person.image} alt="" />
                     <div className="ml-3">
-                        <h3 className="font-bold text-base">Satish Patel</h3>
+                        <h3 className="font-bold text-base">{person.name}</h3>
                         <span className="text-[0.7rem]">Founder & CEO, Huddle</span>
                     </div>
                 </div>
