9a3336494c972ccff4284579c5cf8b0d90508e22
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-11 15:34:57 +0530

    Added basic html for Testimonial component

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,7 +1,17 @@
+import profile1 from "../assets/profile-1.jpg"
 const Testimonial = ()=>{
     return(
         <section>
-            <h1>Testimonial Here</h1>
+            <div>
+                <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.</p>
+                <div>
+                    <img src={profile1} alt="" />
+                    <div>
+                        <h3>Satish Patel</h3>
+                        <span>Founder & CEO, Huddle</span>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
