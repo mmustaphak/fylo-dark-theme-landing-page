77bafaafe37d1c3cced0173d9e4457e6b94c84de
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-11 15:27:38 +0530

    Added Testimonial component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Nav from "./components/Nav.jsx"
 import Hero from "./components/Hero.jsx"
 import AdditionalInfo from "./components/AdditionalInfo.jsx"
 import Collaboration from "./components/Collaboration.jsx"
+import Testimonial from "./components/Testimonial.jsx"
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Hero/>
       <AdditionalInfo/>
       <Collaboration/>
+      <Testimonial/>
     </>
   )
 }
---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,9 @@
+const Testimonial = ()=>{
+    return(
+        <section>
+            <h1>Testimonial Here</h1>
+        </section>
+    )
+}
+
+export default Testimonial
