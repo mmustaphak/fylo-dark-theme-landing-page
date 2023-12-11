e79e8fdab13d20d428d2705885ed8c49e58dbcba
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-12-11 16:20:06 +0530

    used props for individual testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,17 +1,28 @@
 import profile1 from "../assets/profile-1.jpg"
-const Testimonials = ()=>{
-    return(
-        <section className="bg-main text-white px-6 pt-32">
-            <div className="px-6 py-8 bg-testimonial rounded-md">
+import profile2 from "../assets/profile-2.jpg"
+import profile3 from "../assets/profile-3.jpg"
+
+const Testimonials = () => {
+
+    const images = [profile1, profile2, profile3]
+    const testimonials = images.map((image) => {
+        return(
+            <div key={image} className="px-6 py-8 bg-testimonial rounded-md mt-6">
                 <p className="text-sm">Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.</p>
                 <div className="flex items-center mt-4 text-xs">
-                    <img className="w-8 h-8 rounded-full" src={profile1} alt="" />
+                    <img className="w-8 h-8 rounded-full" src={image} alt="" />
                     <div className="ml-3">
                         <h3 className="font-bold text-base">Satish Patel</h3>
                         <span className="text-[0.7rem]">Founder & CEO, Huddle</span>
                     </div>
                 </div>
             </div>
+        )
+    })
+
+    return (
+        <section className="bg-main text-white px-6 pt-32">
+            {testimonials}
         </section>
     )
 }
