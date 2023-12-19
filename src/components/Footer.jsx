6afe5551425c7ef2296f93913aceb5a9c03c31c5
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-19 12:31:42 +0530

    Completed overall styling for Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,34 +8,53 @@ import twitter from "../assets/icon-twitter.svg"
 
 const Footer = () => {
     return (
-        <footer className="bg-footer">
+        <footer className="bg-footer px-5 pt-6 pb-14 text-white">
             <img src={logo} alt="Fylo Logo" />
 
-            <div>
-                <div>
-                    <div>
-                        <img src={pin} alt="Location pin" />
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magnam ut optio accusantium corporis id autem expedita laboriosam dolorum repudiandae debitis voluptatem iure, quam explicabo natus qui sed vitae labore!</p>
-                    </div>
+            <div className="mt-12">
+                <div className="flex">
+                    <img src={pin} className="self-start w-10 mt-1" alt="Location pin" />
+                    <p className="ml-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magnam ut optio accusantium corporis </p>
+                </div>
 
-                    <div>
-                        <img src={phone} alt="Phone" />
-                        <p>+1-543-123-4567</p>
-                    </div>
+                <div className="flex items-center mt-4">
+                    <img src={phone} alt="Phone" />
+                    <p className="ml-4">+1-543-123-4567</p>
+                </div>
 
-                    <div>
-                        <img src={email} alt="envelop" />
-                        <p>example@fylo.com</p>
-                    </div>
+                <div className="flex items-center mt-4">
+                    <img src={email} alt="envelop" />
+                    <p className="ml-4">example@fylo.com</p>
                 </div>
             </div>
 
-            <div>
-                <img src={facebook} alt="" />
-                <img src={instagram} alt="" />
-                <img src={twitter} alt="" />
+            <div className="mt-16 ">
+                <div className="flex flex-col justify-between">
+                    <p className="mt-3">About us</p>
+                    <p className="mt-3">Jobs</p>
+                    <p className="mt-3">Press</p>
+                    <p className="mt-3">Blog</p>
+                </div>
+
+                <div className="flex flex-col justify-between mt-8">
+                    <p className="mt-3">Contact us</p>
+                    <p className="mt-3">Terms</p>
+                    <p className="mt-3">Privacy</p>
+                </div>
             </div>
-            
+
+            <div className="flex justify-between mt-20 mx-auto max-w-[8rem]">
+                <a href="#">
+                    <img src={facebook} className="border rounded-full p-1" alt="Facebook logo" />
+                </a>
+                <a href="#">
+                    <img src={twitter} className="border rounded-full p-1" alt="Twitter logo" />
+                </a>
+                <a href="#">
+                    <img src={instagram} className="border rounded-full p-1" alt="Instagram Logo" />
+                </a>
+            </div>
+
         </footer>
     )
 }
