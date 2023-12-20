bb05aab314866b2c4b5551b4b55a8d28f751c1c7
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-20 11:03:47 +0530

    Added basic html for Signup component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,10 @@
 const SignUp = ()=>{
     return(
         <form>
-            <h1>Sign up here</h1>
+            <h1>Get early access today</h1>
+            <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have nay questions, our support team would happy to help you.</p>
+            <input placeholder="email@example.com"></input>
+            <input type="submit"/>
         </form>
     )
 }
