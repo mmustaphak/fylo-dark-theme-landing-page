f6306126732ab5f6374d05c82e12056231272627
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-20 11:57:04 +0530

    Added Overall Styling to SignUp component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,10 @@
 const SignUp = ()=>{
     return(
-        <form className="bg-main text-center border">
-            <h1>Get early access today</h1>
-            <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have nay questions, our support team would happy to help you.</p>
-            <input placeholder="email@example.com"></input>
-            <input type="submit"/>
+        <form className="bg-main text-center border px-4 py-8">
+            <h1 className="font-bold font-raleway text-lg">Get early access today</h1>
+            <p className="mt-4 w-fit">It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would happy to help you.</p>
+            <input className="block min-w-full mt-8 py-4 rounded-full pl-9 text-black" placeholder="email@example.com"></input>
+            <button className="w-full mt-6 py-2 rounded-full bg-gradient-to-r from-call-to-action-gradient1 to-call-to-action-gradient2">Get Started For Free</button>
         </form>
     )
 }
