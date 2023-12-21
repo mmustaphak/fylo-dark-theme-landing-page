d3a3063759b365f62648d790808eacfc8a4aab56
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-21 12:23:55 +0530

    Capped nav bar width for larger screen sizes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,15 @@
 import logo from "../assets/logo.svg"
-const nav = ()=>{
-    return(
-        <nav className="flex font-raleway items-center justify-between p-4 bg-intro">
-            <img className="w-20" src={logo} alt="" />
-            <ul className="text-white flex justify-between w-[60%] max-w-[15rem]">
-                <li><a href="#">Features</a></li>
-                <li><a href="#">Team</a></li>
-                <li><a href="#">Sign in</a></li>
-            </ul>
+const nav = () => {
+    return (
+        <nav className="font-raleway bg-intro pt-3">
+            <div className="flex items-center justify-between p-4 max-w-[1410px] mx-auto">
+                <img className="w-20" src={logo} alt="" />
+                <ul className="text-white flex justify-between w-[60%] max-w-[15rem]">
+                    <li><a href="#">Features</a></li>
+                    <li><a href="#">Team</a></li>
+                    <li><a href="#">Sign in</a></li>
+                </ul>
+            </div>
         </nav>
     )
 }
