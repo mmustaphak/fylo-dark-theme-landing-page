e41570d59ad76790d65eee228e3f04044e7275f5
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-23 11:33:04 +0530

    Made signup component compatilbe with larger screensizes

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,12 @@
 const SignUp = ()=>{
     return(
-        <form className="absolute min-w-[320px] left-1/2 translate-x-[-50%] translate-y-[-50%] bottom-[-22rem] shadow-[2px_7px_10px_black] bg-intro text-center rounded-lg px-4 py-8">
+        <form className="absolute min-w-[320px] w-3/4 max-w-[850px] left-1/2 translate-x-[-50%] translate-y-[-50%] bottom-[-22rem] shadow-[2px_7px_10px_black] bg-intro text-center rounded-lg px-4 py-8">
             <h1 className="font-bold font-raleway text-lg">Get early access today</h1>
-            <p className="mt-4 w-fit">It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would happy to help you.</p>
-            <input className="block min-w-full mt-8 py-4 rounded-full pl-9 text-black" placeholder="email@example.com"></input>
-            <button className="w-full mt-6 py-2 rounded-full bg-gradient-to-r from-call-to-action-gradient1 to-call-to-action-gradient2">Get Started For Free</button>
+            <p className="mt-4 w-fit max-w-[680px] mx-auto">It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would happy to help you.</p>
+            <div className="flex items-center justify-around flex-wrap">
+                <input className="block w-full max-w-lg mt-8 py-4 rounded-full pl-9 text-black" placeholder="email@example.com"></input>
+                <button className="mt-6 py-4 px-4 rounded-full bg-gradient-to-r from-call-to-action-gradient1 to-call-to-action-gradient2">Get Started For Free</button>
+            </div>
         </form>
     )
 }
