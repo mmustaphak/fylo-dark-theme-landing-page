1526d00ffc35137cc990bb700ae45dbee6a078c5
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-25 11:58:12 +0530

    Configured Color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ export default {
   theme: {
     fontFamily: {
       'raleway': ['Raleway','sans-serif']
+    },
+    extend:{
+      colors:{
+        intro: 'hsl(var(--color-intro) / <alpha-value>)',
+        main: 'hsl(var(--color-main) / <alpha-value>)',
+        footer: 'hsl(var(--color-footer) / <alpha-value>)',
+        testimonial: 'hsl(var(--color-testimonial) / <alpha-value>)',
+        gradient1: 'hsl(var(--color-gradient1) / <alpha-value>)',
+        gradient2: 'hsl(var(--color-gradient2) / <alpha-value>)',
+        error: 'hsl(var(--color-error) / <alpha-value>)'
+      }
     }
   },
   plugins: [],
