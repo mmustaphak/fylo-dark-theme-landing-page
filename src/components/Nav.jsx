9068d8769b2eeb195e3f900c0a804ccb224fc239
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-26 11:45:04 +0530

    Decreased the font size of Nav text

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = ()=>{
         <header className=" font-raleway p-4 bg-intro">
             <nav className="flex justify-between text-white items-center bg-intro">
                 <img src={logo} className="w-24" alt="Fylo" />
-                <ul className="flex">
+                <ul className="flex text-sm">
                     <li><a href="#" target="_blank">Features</a></li>
                     <li className="ml-4"><a href="#" target="_blank">Team</a></li>
                     <li className="ml-4"><a href="#" target="_blank">Sign in</a></li>
