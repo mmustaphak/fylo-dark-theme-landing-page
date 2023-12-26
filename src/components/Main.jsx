f9428aee0fd2ab0ad2c9eb686cb376507c2c2d16
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-26 12:29:25 +0530

    Added basic styling for Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,13 @@ import introImg from "../assets/illustration-intro.png"
 
 const Main = ()=>{
     return(
-        <main>
-            <img src={introImg} className="w-2/3" alt="Organising folder" />
-            <h1>All your files and location, accessible in one secure location anywhere</h1>
-            <p>Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
-            <button>Get started</button>
+        <main className="bg-intro flex flex-col items-center">
+            <img src={introImg} className="w-[80%] max-w-[550px] mt-8" alt="Organising folder" />
+            <div className="w-full flex flex-col items-center bg-main px-9 text-center text-white">
+                <h1 className="font-raleway text-2xl font-bold">All your files in one secure location, accessible anywhere.</h1>
+                <p className="max-w-[437px] mt-4 text-sm">Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
+                <button className="w-3/4 max-w-[220px] mt-8 py-3 px-16 rounded-full bg-gradient-to-r from-gradient1 to-gradient2">Get started</button>
+            </div>
         </main>
     )
 }
