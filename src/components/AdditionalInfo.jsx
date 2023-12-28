73e34d24a14f8fab8ef354f0acf26ca4f71a78f0
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-28 16:16:01 +0530

    Made AdditionalInfo component more declarative and DRY

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -4,32 +4,48 @@ import collaboration from "../assets/icon-collaboration.svg"
 import file from "../assets/icon-any-file.svg"
 
 const AdditionalInfo = () => {
+    const content = [
+        {
+            key: 1,
+            img: access,
+            title: "Access your files, anywhere",
+            content: "The ability to use a smartphone, tablet, or computer to access your account mean your files follow you everywhere."
+        },
+        {
+            key: 2,
+            img: security,
+            title: "Security you can trust",
+            content: "Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required."
+        },
+        {
+            key: 3,
+            img: collaboration,
+            title: "Real-time collaboration",
+            content: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure you files."
+        },
+        {
+            key: 4,
+            img: file,
+            title: "Store any type of file",
+            content: "Whether you're sharing holidays photos or work documents, Fylo has covered allowing for all file types to be securely stored and shared."
+        },
+    ]
+
+    const renderedContent = content.map(item => {
+        return (
+            <div key={item.key} className="mx-auto max-w-[410px]">
+                <img src={item.img} className="mx-auto" alt="" />
+                <h2 className="mt-8 text-xl font-raleway font-bold">{item.title}</h2>
+                <p className="mt-2">{item.content}</p>
+            </div>
+
+        )
+    })
+
     return (
         <section className="pt-32 px-4 text-white bg-main">
             <div className="grid gap-y-24 items-center text-center max-w-[990px] mx-auto min-[900px]:grid-cols-2">
-                <div className="mx-auto max-w-[410px]">
-                    <img src={access} className="mx-auto" alt="" />
-                    <h2 className="mt-8 text-xl font-raleway font-bold">Access your files, anywhere</h2>
-                    <p className="mt-2">The ability to use a smartphone, tablet, or computer to access your account mean your files follow you everywhere.</p>
-                </div>
-
-                <div className="mx-auto max-w-[410px]">
-                    <img src={security} className="mx-auto" alt="" />
-                    <h2 className="mt-8 text-xl font-raleway font-bold">Security you can trust</h2>
-                    <p className="mt-2">2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure you files.</p>
-                </div>
-
-                <div className="mx-auto max-w-[410px]">
-                    <img src={collaboration} className="mx-auto" alt="" />
-                    <h2 className="mt-8 text-xl font-raleway font-bold">Security you can trust</h2>
-                    <p className="mt-2">Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
-                </div>
-
-                <div className="mx-auto max-w-[410px]">
-                    <img src={file} className="mx-auto" alt="" />
-                    <h2 className="mt-8 text-xl font-raleway font-bold">Store any type of file</h2>
-                    <p className="mt-2">Whether you&#39;re sharing holidays photos or work documents, Fylo has covered allowing for all file types to be securely stored and shared.</p>
-                </div>
+                {renderedContent}
             </div>
         </section>
     )
