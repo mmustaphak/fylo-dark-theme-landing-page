c0b9b2e7678c0c0010e9ae903517b8ad694d8681
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2023-12-28 12:06:36 +0530

    Added AdditioalInfo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Nav from "./components/Nav.jsx"
 import Main from "./components/Main.jsx"
+import AdditionalInfo from "./components/AdditionalInfo.jsx"
+
 export default function App() {
   return (
     <>
       <Nav/>
       <Main/>
+      <AdditionalInfo/>
     </>
   )
 }
---
+++ src/components/AdditionalInfo.jsx
@@ -0,0 +1,17 @@
+import access from "../assets/icon-access-anywhere.svg"
+import security from "../assets/icon-security.svg"
+import collaboration from "../assets/icon-collaboration.svg"
+import file from "../assets/icon-any-file.svg"
+
+const AdditioinalInfo = ()=>{
+    return(
+        <section>
+            <img src={access} alt="" />
+            <img src={security} alt="" />
+            <img src={collaboration} alt="" />
+            <img src={file} alt="" />
+        </section>
+    )
+}
+
+export default AdditioinalInfo
